844720c8cf6b4b579fb6e979c6b4fec3794f05ed
isaacsungpak <sungyulpak@gmail.com>
2022-01-28 10:30:43 -0500

    .

--- app/seeds/songs.py
+++ app/seeds/songs.py
@@ -74,15 +74,8 @@ def seed_songs():
     db.session.add(med)
     db.session.add(med)
     db.session.add(nt)
-
     db.session.commit()
 
-
-# Uses a raw SQL query to TRUNCATE the users table.
-# SQLAlchemy doesn't have a built in function to do this
-# TRUNCATE Removes all the data from the table, and RESET IDENTITY
-# resets the auto incrementing primary key, CASCADE deletes any
-# dependent entities
 def undo_songs():
     db.session.execute('TRUNCATE songs RESTART IDENTITY CASCADE;')
     db.session.commit()
